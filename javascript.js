5bb4a1f3322ddd4d31e844c8c9d1f636d5402392
helan <helanmah@gmail.com>
2022-12-14 16:27:34 -0800

    began converting days into factory function

--- javascript.js
+++ javascript.js
@@ -12,3 +12,20 @@ dropDown.addEventListener('click', () => {
         dropDown.innerText = 'v'
     }
 })
+
+/*make days into a factory function with repeatable properties for each day,
+and each day as an item in an array*/
+const days = () => {
+    const day = (number,title,description,completed,video,buttons) => {
+        return {number,title,description,completed,video,buttons}
+    }
+}
+
+
+
+//make account to save progress and backend storage
+//add progress bar
+//add ability to submit videos (go to backend to be approved, then where do they go?)
+//add ability to submit exercises/challenges (for social anx and other topics)
+//add profiles that show your progress and videos you've submitted
+//add points for completing challenges and fun animations
