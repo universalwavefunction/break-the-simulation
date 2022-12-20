e85848195cb1a14b626ed3c46044e7af69b058a4
helan <helanmah@gmail.com>
2022-12-20 12:16:02 -0800

    added factory function to add days

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+/*
 const dropDown = document.getElementById('dropdown')
 const contents = document.getElementById('day-contents')
 contents.style.display = 'flex'
@@ -12,14 +13,93 @@ dropDown.addEventListener('click', () => {
         dropDown.innerText = 'v'
     }
 })
+*/
 
 /*make days into a factory function with repeatable properties for each day,
 and each day as an item in an array*/
-const days = () => {
-    const day = (number,title,description,completed,video,buttons) => {
-        return {number,title,description,completed,video,buttons}
-    }
+function addDay(day) {
+    //container for each day
+    const dayContainer = document.createElement('div');
+
+    //container for the dropdown and title
+    const dropdownContainer = document.createElement('div');
+    const checkbox = document.createElement('input');
+    const title = document.createElement('h2');
+    const dropdown = document.createElement('button');
+
+    //create content for each day
+    const dayContents = document.createElement('div');
+    const video = document.createElement('video');
+    const description = document.createElement('p');
+
+    //create buttons and container
+    const buttonContainer = document.createElement('div');
+    const submitButton = document.createElement('button');
+    const completeButton = document.createElement('button');
+    const discordButton = document.createElement('button');
+    const completedBy = document.createElement('p');
+
+    // Set the content for the elements
+    checkbox.type = 'checkbox';
+    title.textContent = day.title;
+    dropdown.textContent = 'v';
+    description.textContent = day.description;
+    video.src = day.videoUrl;
+    video.type = day.videoType
+    submitButton.textContent = 'Submit Video';
+    completeButton.textContent = 'Mark Complete';
+    discordButton.textContent = 'Discord';
+    completedBy.textContent = day.completedBy;
+
+    // Add the elements to the container elements
+    dropdownContainer.appendChild(checkbox);
+    dropdownContainer.appendChild(title);
+    dropdownContainer.appendChild(dropdown);
+    dayContainer.appendChild(dropdownContainer);
+    dayContents.appendChild(description);
+    dayContents.appendChild(video);
+    dayContainer.appendChild(dayContents);
+    buttonContainer.appendChild(submitButton);
+    buttonContainer.appendChild(completeButton);
+    buttonContainer.appendChild(discordButton);
+    buttonContainer.appendChild(completedBy);
+    dayContainer.appendChild(buttonContainer);
+
+    // Return the container element
+    return dayContainer;
 }
+
+// Array of objects with the content for each item
+const days = [
+    {
+      title: 'Day 1',
+      description: `Say hi to a random stranger<br>
+      while walking anywhere<br>`,
+      videoUrl: 'saying-hi-to-strangers.mp4',
+      videoType: 'video/mp4',
+      completedBy: 'Completed by: 3'
+    },
+    {
+      title: 'Day 2',
+      description: 'This is the second day',
+      videoUrl: '',
+      videoType: '',
+      completedBy: 'Completed by: 3'
+    },
+    {
+      title: 'Day 3',
+      description: 'This is the third day',
+      videoUrl: '',
+      videoType: '',
+      completedBy: 'Completed by: 3'
+    },
+  ];
+
+  // Iterate over the items array and create the HTML elements using the factory function
+  for (const day of days) {
+    const dayElement = addDay(day);
+    document.getElementById('days-list').appendChild(dayElement);
+  }
 
 
 
